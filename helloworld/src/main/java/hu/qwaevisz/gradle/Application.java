3b4339edce5aa5dff6fb4d065aa235eaaae832a9
edvibed <david.bedok@ericsson.com>
2015-09-10 09:26:16 +0200

    ADD: helloworld project

---
+++ helloworld/src/main/java/hu/qwaevisz/gradle/Application.java
@@ -0,0 +1,13 @@
+package hu.qwaevisz.gradle;
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+}
