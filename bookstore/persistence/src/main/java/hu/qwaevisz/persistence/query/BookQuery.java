fefa785bdb481e7c7705d4224408f0d9ef1c6a23
edvibed <david.bedok@ericsson.com>
2015-09-24 13:56:30 +0200

    ADD: BookStore - minor sql scripts and minor classes

---
+++ bookstore/persistence/src/main/java/hu/qwaevisz/persistence/query/BookQuery.java
@@ -0,0 +1,9 @@
+package hu.qwaevisz.persistence.query;
+
+public class BookQuery {
+
+	public static final String GET_BY_ID = "Book.getById";
+	public static final String GET_BY_ISBN = "Book.getByIsbn";
+	public static final String GET_ALL = "Book.getAll";
+
+}
