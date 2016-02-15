b70247c12c17c2599b2b9c4be864e897f479186c
Dávid Bedők <david@bedok.hu>
2016-02-15 21:01:20 +0100

    ADD: sh scripts for hellotest

---
+++ hellotest/compile.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+javac -d ./bin ./src1/hu/qwaevisz/demo/HelloWorld.java ./src2/hu/qwaevisz/demo/HelloTest.java
---
+++ hellotest/createjar.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd bin
+jar cvfe hellotest.jar hu.qwaevisz.demo.HelloWorld hu/qwaevisz/demo/HelloTest.class hu/qwaevisz/demo/HelloWorld.class
